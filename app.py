a377d156f9f62384b2274c4a31aa0980c5999add
vitupreys1 <leaguegjm@gmail.com>
2025-06-04 11:06:44 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__) 
+app.json.ensure_ascii = False
+
+
+jogos = [
+    {"id": 1, "titulo": "The Last of Us", "genero": "Ação", "ano_lancamento": 2013},
+    {"id": 2, "titulo": "Detroit: Become Human", "genero": "Aventura", "ano_lancamento": 2018},
+    {"id": 3, "titulo": "God of War", "genero": "Ação", "ano_lancamento": 2018},
+    {"id": 4, "titulo": "Devil May Cry 5", "genero": "Ação", "ano_lancamento": 2019}
+]
+
+# Variável para armazenar o ID do próximo jogo 
+next_id = max([jogo['id'] for jogo in jogos]) + 1 if jogos else 1
+
+
+
+
+# 1. Rota raiz  retornando uma mensagem
+@app.route('/')
+def home():
+   
+    return jsonify({ 'mensagem':'Bem-vindo à minha API de jogos de videogame!'})
+
+@app.route('/dados', methods=['GET'])
+def get_dados_jogos():
+    
+    return jsonify(jogos)
+
+
+
+# 3. Obter um jogo por ID
+@app.route('/jogos/<int:jogo_id>', methods=['GET'])
+def get_jogo_por_id(jogo_id):
+    for jogo in jogos:
+        if jogo["id"] == jogo_id:
+            return jsonify(jogo)
+    return jsonify({"erro": "Jogo não encontrado"}), 404
+
+# 4. Adicionar um novo jogo
+
+
+if __name__ == '__main__':
+    app.run(debug=True) 
